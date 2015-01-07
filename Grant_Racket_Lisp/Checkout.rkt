340360d2d603a22ed4ce54b7b3ca676c255c3ad8
Grant Crofton <grantac@hotmail.com>
2015-01-07 18:00:20 +0000

    Further lisp

--- Grant_Racket_Lisp/Checkout.rkt
+++ Grant_Racket_Lisp/Checkout.rkt
@@ -1,6 +1,26 @@
-#lang racket/base
+#lang racket
 
 (define (calc-total items prices)
   (* (length items) prices))
 
-(provide calc-total)
+(define (lookup-price item prices)
+  (hash-ref prices item))
+
+(define (sum-items items prices)
+  (if (null? items)
+     0
+     (+ (lookup-price (first items) prices) (sum-items (rest items) prices))
+   )
+  )
+
+(define (calc-total2 items prices)
+  (cond
+    [(= (length items) 0) 
+     0]
+    [else 
+     (sum-items items prices)
+     ;(* (length items) (hash-ref prices (list-ref items 0)))
+     ]
+    ))
+
+(provide calc-total calc-total2)
--- Grant_Racket_Lisp/CheckoutTests.rkt
+++ Grant_Racket_Lisp/CheckoutTests.rkt
@@ -1,18 +1,26 @@
-#lang racket/base
+#lang racket
 
-; "Apple" $0.2
-; Banana" $0.4 or 3 for $1
+; Apple $0.3 or 4 for $1
+; Banana $0.5
+; Coke $1.80
+; Deodorant $2.50 or 2 for $4.50
 
 (require rackunit
          "Checkout.rkt")
 
-(check-equal? (calc-total '() 0.2) 0 "Empty list gives zero")
+(define prices 
+  (hash
+   null '0.0
+   "Apple" '0.2
+   "Banana" '0.4))
 
-(check-equal? (calc-total '("Apple") 0.2) 0.2 "Single apple gives total")
+(check-equal? (calc-total2 '() prices) 0 "Empty list gives zero")
 
-(check-equal? (calc-total '("Apple" "Apple") 0.2) 0.4 "Multiple apple gives total")
+(check-equal? (calc-total2 '("Apple") prices) 0.2 "Single apple gives total")
 
-(check-equal? (calc-total '("Banana") '(0.2 0.4)) 0.4 "Single banana gives total")
+(check-equal? (calc-total2 '("Apple" "Apple") prices) 0.4 "Multiple apple gives total")
+
+(check-equal? (calc-total2 '("Banana") prices) 0.4 "Single banana gives total")
 
 "**********************"
 "*** Tests Complete ***"
