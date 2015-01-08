ac0a53d5b211ef4b22b91819b68ca51ecde09cd4
Grant Crofton <grantac@hotmail.com>
2015-01-08 10:39:39 +0000

    A bit more Lisp

--- Grant_Racket_Lisp/Checkout.rkt
+++ Grant_Racket_Lisp/Checkout.rkt
@@ -1,26 +1,23 @@
 #lang racket
 
-(define (calc-total items prices)
-  (* (length items) prices))
-
 (define (lookup-price item prices)
   (hash-ref prices item))
 
-(define (sum-items items prices)
+(define (calc-undiscounted-price items prices)
   (if (null? items)
      0
-     (+ (lookup-price (first items) prices) (sum-items (rest items) prices))
-   )
-  )
+     (+ 
+      (lookup-price (first items) prices) 
+      (calc-undiscounted-price (rest items) prices))))
 
-(define (calc-total2 items prices)
-  (cond
-    [(= (length items) 0) 
-     0]
-    [else 
-     (sum-items items prices)
-     ;(* (length items) (hash-ref prices (list-ref items 0)))
-     ]
-    ))
+(define (calculate-discount items)
+  (if (equal? items '("Deodorant" "Deodorant"))
+      0.5
+      0))
 
-(provide calc-total calc-total2)
+(define (calc-total items prices)
+  (-
+   (calc-undiscounted-price items prices)
+   (calculate-discount items)))
+
+(provide calc-total)
--- Grant_Racket_Lisp/CheckoutTests.rkt
+++ Grant_Racket_Lisp/CheckoutTests.rkt
@@ -11,16 +11,23 @@
 (define prices 
   (hash
    null '0.0
-   "Apple" '0.2
-   "Banana" '0.4))
+   "Apple" '0.3
+   "Banana" '0.5
+   "Coke" '1.8
+   "Deodorant" '2.5
+   ))
 
-(check-equal? (calc-total2 '() prices) 0 "Empty list gives zero")
+(check-equal? (calc-total '() prices) 0 "Empty list gives zero")
 
-(check-equal? (calc-total2 '("Apple") prices) 0.2 "Single apple gives total")
+(check-equal? (calc-total '("Apple") prices) 0.3 "Single apple gives total")
 
-(check-equal? (calc-total2 '("Apple" "Apple") prices) 0.4 "Multiple apple gives total")
+(check-equal? (calc-total '("Apple" "Apple") prices) 0.6 "Multiple apple gives total")
 
-(check-equal? (calc-total2 '("Banana") prices) 0.4 "Single banana gives total")
+(check-equal? (calc-total '("Banana") prices) 0.5 "Single banana gives total")
+
+(check-equal? (calc-total '("Apple" "Banana" "Coke" "Deodorant") prices) (+ 0.3 0.5 1.8 2.5) "All single prices work")
+
+(check-equal? (calc-total '("Deodorant" "Deodorant") prices) 4.5 "Single discount works")
 
 "**********************"
 "*** Tests Complete ***"
