11310b1e4f177ccb5300a90c189ffdb06f038d43
Grant Crofton <grantac@hotmail.com>
2015-02-05 14:06:33 +0000

    Extended Scheme solution a bit

--- Grant_Racket_Lisp/Checkout.rkt
+++ Grant_Racket_Lisp/Checkout.rkt
@@ -1,23 +1,49 @@
 #lang racket
 
+; calculates total by taking discount from prices
+(define (calculate-total items prices discounts)
+  (- (calculate-undiscounted-price items prices)
+     (calculate-discount items discounts)))
+
+; adds all of the prices up
+(define (calculate-undiscounted-price items prices)
+  (cond 
+    ((null? items) 0)
+    (else (+ (lookup-price (first items) prices) 
+             (calculate-undiscounted-price (rest items) prices)))))
+
+; adds all of the discounts up
+(define (calculate-discount items discounts)
+  (cond
+    ((null? discounts) 0)
+    (else (+ (discount-for items (car discounts))
+             (calculate-discount items (cdr discounts))))))
+
+; gets the discount value for an individual discount
+(define (discount-for items discount)
+  (cond
+    ((eq? (count-item-occurances (discount-item discount) items)
+          (item-count discount))
+     (discount-value discount))
+    (else 0)))
+
+; helpers
 (define (lookup-price item prices)
   (hash-ref prices item))
 
-(define (calc-undiscounted-price items prices)
-  (if (null? items)
-     0
-     (+ 
-      (lookup-price (first items) prices) 
-      (calc-undiscounted-price (rest items) prices))))
+(define (discount-item discount)
+  (car discount))
 
-(define (calculate-discount items)
-  (if (equal? items '("Deodorant" "Deodorant"))
-      0.5
-      0))
+(define (item-count discount)
+  (caadr discount))
 
-(define (calc-total items prices)
-  (-
-   (calc-undiscounted-price items prices)
-   (calculate-discount items)))
+(define (discount-value discount)
+  (car (cdr (car (cdr discount)))))
 
-(provide calc-total)
+(define (count-item-occurances item items)
+  (cond
+    ((null? items) 0)
+    ((eq? (car items) item) (+ 1 (count-item-occurances item (cdr items))))
+    (else (count-item-occurances item (cdr items)))))
+  
+(provide calculate-total)
--- Grant_Racket_Lisp/CheckoutTests.rkt
+++ Grant_Racket_Lisp/CheckoutTests.rkt
@@ -10,24 +10,52 @@
 
 (define prices 
   (hash
-   null '0.0
-   "Apple" '0.3
-   "Banana" '0.5
-   "Coke" '1.8
-   "Deodorant" '2.5
-   ))
+   null 0.0
+   "Apple" 30
+   "Banana" 50
+   "Coke" 180
+   "Deodorant" 250))
 
-(check-equal? (calc-total '() prices) 0 "Empty list gives zero")
+(define discounts
+  '(("Apple" (4 20))
+    ("Deodorant" (2 50))))
 
-(check-equal? (calc-total '("Apple") prices) 0.3 "Single apple gives total")
+(define (priceof item)
+  (hash-ref prices item))
 
-(check-equal? (calc-total '("Apple" "Apple") prices) 0.6 "Multiple apple gives total")
+(check-equal? (calculate-total '() prices discounts) 
+              0 
+              "Empty list gives zero")
 
-(check-equal? (calc-total '("Banana") prices) 0.5 "Single banana gives total")
+(check-equal? (calculate-total '("Apple") prices discounts) 
+              (priceof "Apple") 
+              "Single apple gives total")
 
-(check-equal? (calc-total '("Apple" "Banana" "Coke" "Deodorant") prices) (+ 0.3 0.5 1.8 2.5) "All single prices work")
+(check-equal? (calculate-total '("Apple" "Apple") prices discounts) 
+              (* 2 (priceof "Apple")) 
+              "Multiple apple gives total")
 
-(check-equal? (calc-total '("Deodorant" "Deodorant") prices) 4.5 "Single discount works")
+(check-equal? (calculate-total '("Banana") prices discounts) 
+              (priceof "Banana") 
+              "Single banana gives total")
+
+(check-equal? (calculate-total '("Apple" "Banana" "Coke" "Deodorant") prices discounts) 
+              (+ (priceof "Apple") (priceof "Banana") (priceof "Coke") (priceof "Deodorant")) 
+              "All single prices work")
+
+(check-equal? (calculate-total '("Apple" "Apple" "Apple" "Apple") prices discounts) 
+              100 
+              "Apple discount works")
+
+(check-equal? (calculate-total '("Deodorant" "Deodorant") prices discounts) 
+              450 
+              "Deodorant discount works")
+
+(check-equal? (calculate-total '("Apple" "Apple" "Deodorant" "Apple" "Deodorant" "Apple" ) prices discounts) 
+              550 
+              "Multiple discounts work for unordered items")
+
+; next: same discount multiple times
 
 "**********************"
 "*** Tests Complete ***"
